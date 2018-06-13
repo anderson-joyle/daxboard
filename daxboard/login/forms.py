5af29c0fa3d5d05443c9975a7c7b5fafd8b1d8d9
Anderson Joyle <anderson@joyle.com.br>
2018-06-14 01:19:22 +0200

    Fixing content.

--- daxboard/login/forms.py
+++ daxboard/login/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    dynamics_fo_url = forms.URLField(max_length=256, label='D365 FO URL', required=False, widget=forms.TextInput(attrs={'class': "form-control", 'placeholder': 'D36r URL address'}))
+    dynamics_fo_url = forms.URLField(max_length=256, label='D365 FO URL', required=False, widget=forms.TextInput(attrs={'class': "form-control", 'placeholder': 'D365 URL address'}))
     tenant = forms.CharField(label='Tenant', required=False, widget=forms.TextInput(attrs={'class': "form-control", 'placeholder': 'Azure AD tenant'}))
     client_id = forms.CharField(label='Client id', required=False, widget=forms.TextInput(attrs={'class': "form-control", 'placeholder': 'Azure AD client id'}))
     client_secret = forms.CharField(label='Client secret', required=False, widget=forms.PasswordInput(attrs={'class': "form-control", 'placeholder': 'Azure AD client secret'}))
